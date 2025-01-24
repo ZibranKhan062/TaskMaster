4bfd79a4a5f669319dd517e66d1896cd936b5f95
zibrankhan <zibrangni@gmail.com>
2025-01-24 17:31:12 +0530

    Initial Commit

---
+++ first_project/first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_projectApp.swift
+//  first_project
+//
+//  Created by Zibran Khan on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ first_projectTests/first_projectTests.swift
@@ -0,0 +1,16 @@
+//
+//  first_projectTests.swift
+//  first_projectTests
+//
+//  Created by Zibran Khan on 24/01/25.
+//
+
+import Testing
+
+struct first_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
